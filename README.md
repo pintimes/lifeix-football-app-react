eb14f49cf908a8d90aac9584ecb9af91d4dc2621
NeoYin <neoyin@NeoYin-MacBook-Pro.local>
2016-05-09 10:23:46 +0800

    add react-native tabbers

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # -lifeix-football-app-react
+
+#### 使用
+
+- <https://github.com/ngstyle/react-native-tabBar> 
+
+`npm install react-native-xtabbar --save`
--- news/sources/view/home.js
+++ news/sources/view/home.js
@@ -13,21 +13,59 @@ import {
     Text
 } from 'react-native';
 
+import TabBar from 'react-native-xtabbar';  
+
 class Home extends Component {
 
 	render() {
-		return this.renderLoadingView();
+		return this.runderTabbar();
 	}
 
 	renderLoadingView() {
-	    return (
-	      <View style={styles.container}>
-		    <Text style={styles.title}>
-		      主页
-		    </Text>
-		  </View>
-	    );
-  	}
+    return (
+      <View style={styles.container}>
+	    <Text style={styles.title}>
+	      主页
+	    </Text>
+	  </View>
+    );
+	}
+
+  runderTabbar(){
+    return (
+      <TabBar style={styles.content}>
+          <TabBar.Item
+              icon={require('../imgs/start_normal.png')}
+              selectedIcon={require('../imgs/start_hightlight.png')}
+              onPress={() => {
+                  console.log("first onPress");
+              }}
+              title='首页'>
+              <View style={styles.text}>
+                  <Text style={{fontSize: 18}}>Home</Text>
+              </View>
+          </TabBar.Item>
+
+          <TabBar.Item
+              icon={require('../imgs/start_normal.png')}
+              selectedIcon={require('../imgs/start_hightlight.png')}
+              title='发现'>
+              <View style={styles.text}>
+                  <Text style={{fontSize: 18}}>Find</Text>
+              </View>
+          </TabBar.Item>
+
+          <TabBar.Item
+              icon={require('../imgs/start_normal.png')}
+              selectedIcon={require('../imgs/start_hightlight.png')}
+              title='我'>
+              <View style={styles.text}>
+                  <Text style={{fontSize: 18}}>Me</Text>
+              </View>
+          </TabBar.Item>
+      </TabBar>
+    );
+  }
 
 }
 
