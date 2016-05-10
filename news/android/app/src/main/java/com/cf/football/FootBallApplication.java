c6a6db0d323b8b536b04c44e81b6644b4d940652
Neo <neoy@staff.l99.com>
2016-05-10 19:45:15 +0800

    add umeng android

---
+++ news/android/app/src/main/java/com/cf/football/FootBallApplication.java
@@ -0,0 +1,9 @@
+package com.cf.football;
+
+import com.liuchungui.react_native_umeng_push.UmengPushApplication;
+
+/**
+ * Created by neoyin on 16-5-10.
+ */
+public class FootBallApplication extends UmengPushApplication {
+}
